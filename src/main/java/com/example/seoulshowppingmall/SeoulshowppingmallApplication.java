b142341c44a4afd9e85ca2319dc93f7cf287dc75
100jun001 <jwcom97@naver.com>
2025-05-19 10:36:03 +0900

    first commit

---
+++ src/main/java/com/example/seoulshowppingmall/SeoulshowppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoulshowppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulshowppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulshowppingmallApplication.class, args);
+    }
+
+}
